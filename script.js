d324b100a728f8c3041389d95e62be21f2d24c66
I A <ilie.adrian1986@gmail.com>
2023-12-17 11:54:54 +0200

    html content finished

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+let elapsingtime = document.getElementById("elapsing-time");
+
+function setElapsingTime() {
+    elapsingtime.innerText = "1h:32m:43s";
+}
+
+setElapsingTime()
